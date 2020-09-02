51d030f89c404db34cf7f33a034984962574087e
Daniel <danisolo91@gmail.com>
2020-09-02 23:40:31 +0200

    HTML skeleton with Materialize

--- src/DOMController.js
+++ src/DOMController.js
@@ -1,7 +1,12 @@
+import 'materialize-css';
+import 'materialize-css';
 import ProjectController from './ProjectController';
 
 const DOMController = (() => {
     const renderProjects = () => {
+        var elem = document.querySelector('.collapsible');
+        let instance = M.Collapsible.init(elem);
+
         const projects = ProjectController.getProjects();
         console.log(projects);
     }
